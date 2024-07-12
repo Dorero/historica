842ec8c3e2322288d1c5db98b2a24960107f1305
Ilya Dyudyaev <domenoer@gmail.com>
2024-07-12 08:37:03 +0300

    Fix ci.yaml

--- app/service/jwt_service.rb
+++ app/service/jwt_service.rb
@@ -2,7 +2,8 @@
 
 class JwtService
   JWT_SECRET = Rails.application.credentials.secret_key_base
-
+  p JWT_SECRET
+  p Rails.env
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
 
