36f6037e598f482d6673ec7444e1f40b446d7fb2
Ilya Dyudyaev <domenoer@gmail.com>
2024-07-13 11:19:01 +0300

    Add collection of user reviews

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe "Users", type: :request do
       tags 'Users'
       produces 'application/json'
       parameter name: :authorization, in: :header, type: :string, required: true, description: 'Authorization token'
-      parameter name: :id, in: :path, type: :integer, description: 'Id of the user'
+      parameter name: :id, in: :path, type: :integer, required: true
 
       response '200', "Show user" do
         schema type: :object,
@@ -73,6 +73,52 @@ RSpec.describe "Users", type: :request do
       end
 
       response '404', "User not found" do
+        let(:id) { -1 }
+
+        run_test! do |response|
+          expect(response).to have_http_status(:not_found)
+        end
+      end
+    end
+  end
+
+  path "/users/{id}/reviews" do
+    get "Collection of user reviews" do
+      tags 'Users'
+      produces 'application/json'
+      parameter name: :authorization, in: :header, type: :string, required: true, description: 'Authorization token'
+      parameter name: :id, in: :path, type: :integer, required: true
+
+      response "200", "Collection reviews" do
+        schema type: :array,
+               items: {
+                 type: :object,
+                 properties: {
+                   id: { type: :integer },
+                   title: { type: :string },
+                   content: { type: :string },
+                   user_id: { type: :integer },
+                   place_id: { type: :integer },
+                   created_at: { type: :string, format: 'date-time' },
+                   updated_at: { type: :string, format: 'date-time' }
+                 },
+                 required: ['id', 'title', 'content', 'user_id', 'place_id', 'created_at', 'updated_at']
+               }
+
+        let(:place) { create(:place) }
+        before { create_list(:review, 2, user_id: user.id, place_id: place.id) }
+        let(:id) { user.id }
+
+        run_test! do |response|
+          data = JSON(response.body)
+          expect(data.size).to eq(2)
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      response '404', "User not found" do
+        schema type: :string, example: "User doesn't exist"
+
         let(:id) { -1 }
 
         run_test! do |response|
